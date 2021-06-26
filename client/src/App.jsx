4f978348309c78eab97b59c0f69288b44f084a67
Vijay Guttula <ssvijayg@gmail.com>
2021-06-26 16:51:04 +0530

    Routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home, RestaurantDetailPage, UpdatePage } from './routes';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/restaurants/:id/update' component={UpdatePage} />
+        <Route exact path='/restaurants/:id' component={RestaurantDetailPage} />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/routes/index.js
@@ -0,0 +1,5 @@
+import Home from './Home';
+import RestaurantDetailPage from './RestaurantDetailPage';
+import UpdatePage from './UpdatePage';
+
+export { Home, RestaurantDetailPage, UpdatePage };
