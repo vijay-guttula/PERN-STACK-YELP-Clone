b768797641cf7fdc520133f2201c81c6e7a09e30
Vijay Guttula <ssvijayg@gmail.com>
2021-07-02 11:10:29 +0530

    Reviews Component

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import RestaurantDetail from '../components/RestaurantDetail';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantsContext } from '../context/RestaurantsContext';
+import Reviews from '../components/Reviews';
 
 const RestaurantDetailPage = () => {
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectedRestaurant } =
+    useContext(RestaurantsContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        // console.log(response.data.data.restaurant);
+        setSelectedRestaurant(response.data.data.restaurant);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <RestaurantDetail />
+      {selectedRestaurant && (
+        <>
+          <div className='mt-3'>
+            <Reviews />
+          </div>
+        </>
+      )}
     </div>
   );
 };
