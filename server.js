7564f9cbc7fc5a1478819b0ab5bd40a32ad14947
Vijay Guttula <ssvijayg@gmail.com>
2021-07-03 20:26:34 +0530

    serve.js config

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const restaurantRouter = require('./routes/restaurantRouter');
+const path = require('path');
 
 const app = express();
 
 // CORS
 app.use(cors());
 
-// app.use(bodyParser.json());
+// Body parsing i.e for req.body
 app.use(express.json());
 
 // morgan middleware
 const morgan = require('morgan');
 app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
 // linking routes
 app.use('/api/v1/restaurants', restaurantRouter);
--- server/db/db.sql
+++
@@ -1,9 +0,0 @@
-create table reviews(
-  id bigserial not null primary key,
-  restaurant_id BIGINT not null references restaurants(id),
-  name VARCHAR(50) not null,
-  review text not null,
-  rating int not null check (rating >= 1 and rating <= 5)
-);
-
-SELECT * from restaurants left join (select restaurant_id, count(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id;
