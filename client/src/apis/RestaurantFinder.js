681d9c690485d424d331b7dafa91802db112a04a
Vijay Guttula <ssvijayg@gmail.com>
2021-07-04 15:51:35 +0530

    Heroku

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -5,5 +5,5 @@ export default axios.create({
   baseURL:
     process.env.NODE_ENV !== 'production'
       ? 'http://localhost:3000/api/v1/restaurants'
-      : 'https://pernstack-yelpclone.herokuapp.com',
+      : 'https://pernstack-yelpclone.herokuapp.com/api/v1/restaurants',
 });
