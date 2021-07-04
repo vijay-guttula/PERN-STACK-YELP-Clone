63aec322f1cd9f585709a904e3e85435534080c6
Vijay Guttula <ssvijayg@gmail.com>
2021-07-04 16:12:12 +0530

    SSL easy production/local

--- db/index.js
+++ db/index.js
@@ -5,9 +5,7 @@ const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPAS
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = {
