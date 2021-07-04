e0acb3a127418b96f5abf1535c1aa4b659ad389a
Vijay Guttula <ssvijayg@gmail.com>
2021-07-04 16:05:46 +0530

    Heroku

--- db/index.js
+++ db/index.js
@@ -1,21 +1,14 @@
-const { Pool } = require('pg');
 require('dotenv').config();
+const { Pool } = require('pg');
+const isProduction = process.env.NODE_ENV === 'production';
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
-const devConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-};
-
-const proConfig = {
-  connectionString: process.env.DATABASE_URL, // its going to be coming from heroku addon
-};
-
-const pool = new Pool(
-  process.env.NODE_ENV === 'production' ? proConfig : devConfig
-);
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
